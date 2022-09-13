25d410df1095493c8a185d0705513f2781cb2ef3
brunolimaptr <brunolima.ptr@gmail.com>
2022-09-13 13:20:43 -0300

    projeto

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -5,3 +5,12 @@ const array = [
     [20, 33, 11, 12, 19],
     [3, 3, 4, 5, 10]
 ]
+
+for(let i = 0; i < array.length; i++){
+console.log(`jogador 1: ${array[0]}
+jogador 2: ${array[1]}
+jogador 3: ${array[2]}
+jogador 4: ${array[3]}
+jogador 5: ${array[4]}`)
+}
+
---
+++ exercicio3/script.js
@@ -0,0 +1,10 @@
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"];
+
+for(let i of maioresPaises){
+    
+    console.log(`1 - ${maioresPaises[0]}
+2 - ${maioresPaises[1]}
+3 - ${maioresPaises[2]}
+4 - ${maioresPaises[3]}
+5 - ${maioresPaises[4]}`)
+}
